619c70d6c987182df94a3a33d2371c74c610c333
arturwyroslak <95839215+arturwyroslak@users.noreply.github.com>
2025-08-18 06:13:24 +0200

    Update octokit.ts

--- src/octokit.ts
+++ src/octokit.ts
@@ -2,7 +2,10 @@ import { App } from "@octokit/app";
 import { Octokit } from "octokit";
 import { cfg } from "./config.js";
 
-const app = new App({ appId: cfg.appId, privateKey: cfg.privateKey });
+const app = new App({
+  appId: Number(cfg.appId),          // upewnij się, że to number
+  privateKey: cfg.privateKey         // teraz istnieje w cfg
+});
 
 export async function getInstallationOctokit(installationId: number | string) {
   return await app.getInstallationOctokit(Number(installationId)) as unknown as Octokit;
