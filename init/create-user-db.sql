c04cf38dc101976db61471c3122396a98d1f5329
arturwyroslak <95839215+arturwyroslak@users.noreply.github.com>
2025-08-18 10:35:08 +0200

    Update create-user-db.sql

--- init/create-user-db.sql
+++ init/create-user-db.sql
@@ -1,18 +1,16 @@
-DO
-$$
+DO $$
 BEGIN
   IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'gitautonomic') THEN
     CREATE ROLE gitautonomic WITH LOGIN PASSWORD '12345678';
-    ALTER ROLE gitautonomic CREATEDB;
+  ELSE
+    ALTER ROLE gitautonomic WITH LOGIN PASSWORD '12345678';
   END IF;
 END
 $$;
 
-DO
-$$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'gitautonomic') THEN
-    CREATE DATABASE gitautonomic OWNER gitautonomic;
-  END IF;
-END
-$$;
+-- Zezwól na połączenia do bazy "postgres"
+GRANT CONNECT ON DATABASE postgres TO gitautonomic;
+
+-- Przejdź do bazy "postgres" i nadaj uprawnienia do schematu public
+\connect postgres
+GRANT USAGE, CREATE ON SCHEMA public TO gitautonomic;
